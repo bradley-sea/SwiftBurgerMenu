4714f88bb19622bbff25e936e0c0aee4a73c2282
bradleypj@icloud.com <bradleypj@icloud.com>
2014-09-04 08:55:55 -0700

    first commit

---
+++ burgermenu/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  burgermenu
+//
+//  Created by Bradley Johnson on 9/4/14.
+//  Copyright (c) 2014 learnswift. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
